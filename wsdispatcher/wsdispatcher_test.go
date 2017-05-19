7491ac94645e428dc0ff2c6159267c9e2f5b879f
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2017-05-19 15:19:50 +0200

    Tests

--- wsdispatcher/wsdispatcher_test.go
+++ wsdispatcher/wsdispatcher_test.go
@@ -18,16 +18,14 @@ var (
 
 // Echo is a dummy type that reads a message, wait for some time and sends ret back
 type Echo struct {
-	pong string
-	c    chan []byte
+	c chan []byte
 }
 
 // NewCortex is filling the  ...
 func NewCortex(ctx context.Context) (GetInfoFromCortexFunc, SendInfoToCortex) {
 	c := make(chan []byte)
 	echo := &Echo{
-		pong: "pong",
-		c:    c,
+		c: c,
 	}
 	return echo.Get, echo.Receive
 }
@@ -39,7 +37,7 @@ func (e *Echo) Get(ctx context.Context) chan []byte {
 
 // Receive ...
 func (e *Echo) Receive(ctx context.Context, b *[]byte) {
-	e.c <- []byte(e.pong)
+	e.c <- *b
 }
 func init() {
 	router := mux.NewRouter().StrictSlash(true)
@@ -75,39 +73,10 @@ func TestPingPong(t *testing.T) {
 	}
 }
 
-/*
-func TestServeWs(t *testing.T) {
-	httpURL := url.URL{Scheme: tsURL.Scheme, Host: tsURL.Host, Path: "/serveWs/"}
-	// Try to connect to a socket without an ID
-	request, err := http.NewRequest("GET", httpURL.String(), nil)
-
-	res, err := http.DefaultClient.Do(request)
-
-	if err != nil {
-		t.Error(err)
-	}
-
-	// We don't serve the baseurl, a tag is mandatory
-	if res.StatusCode != 404 {
-		t.Errorf("Success expected: %d", res.StatusCode)
-	}
-
-	//Try with a valid tag
-	httpURL.Path = "/serveWs/1234'"
-	request, err = http.NewRequest("GET", httpURL.String(), nil)
-
-	res, err = http.DefaultClient.Do(request)
-
-	if err != nil {
-		t.Error(err)
-	}
-
-	// We shall get a bad request as we are expected a websocket
-	if res.StatusCode != 200 {
-		t.Errorf("Success expected: %d", res.StatusCode)
-	}
+func TestServeWS(t *testing.T) {
 	// Now test the websocket
-	wsURL := url.URL{Scheme: "ws", Host: tsURL.Host, Path: "/serveWs/1234"}
+	test := []byte("test")
+	wsURL := url.URL{Scheme: "ws", Host: tsURL.Host, Path: "/ws"}
 	c, _, err := websocket.DefaultDialer.Dial(wsURL.String(), nil)
 	if err != nil {
 		t.Errorf("Cannot connect to the websocket %v", err)
@@ -122,41 +91,21 @@ func TestServeWs(t *testing.T) {
 		if err != nil {
 			t.Errorf("Error in the message reception: %v (type %v)", err, tm)
 		}
-		t.Logf("Received message %s of type %v", message, tm)
+		//t.Log("Received message %s of type %v", message, tm)
+		if string(message) != string(test) {
+			t.Fatal("Message received should be the same as the message sent")
+		}
 		done <- true
 	}()
-	// Sending a message with a Set method that will return success
-	type inputOK struct {
-		ID int `json:"id"`
+
+	err = c.WriteMessage(websocket.TextMessage, test)
+	if err != nil {
+		t.Errorf("Cannot write %v to websocket: %v", test, err)
 	}
 
-	messageOK := &inputOK{ID: 0}
-	b, err := json.Marshal(messageOK)
-	if err != nil {
-		t.Error(err)
-	}
-	err = c.WriteMessage(websocket.TextMessage, b)
-	if err != nil {
-		t.Errorf("Cannot write messageOK %v (%v) to websocket: %v", messageOK, b, err)
-	}
-	// Sending a message with a Set method that will return failure
-	type inputKO struct {
-		ID string `json:"id"`
-	}
-
-	messageKO := &inputKO{ID: "ko"}
-	b, err = json.Marshal(messageKO)
-	if err != nil {
-		t.Error(err)
-	}
-	err = c.WriteMessage(websocket.TextMessage, b)
-	if err != nil {
-		t.Errorf("Cannot write messageKO %v (%v) to websocket: %v", messageKO, b, err)
-	}
 	<-done
 	err = c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseGoingAway, ""))
 	if err != nil {
 		t.Errorf("write close: %v", err)
 	}
 }
-*/
